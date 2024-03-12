603c4e17020ab0f0f0bac89962bffb6e9936e746
itzjustfitb <fakhrigajar@gmail.com>
2024-03-12 22:07:13 +0400

    night mode was modified

--- src/Components/NightModeBtn.jsx
+++ src/Components/NightModeBtn.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function NightModeBtn({ nightModeActive, setNightModeActive }) {
+function NightModeBtn() {
+  const getTheme = () => {
+    return JSON.parse(localStorage.getItem("night-mode")) || false;
+  };
+  const [theme, setTheme] = useState(getTheme());
+
+  useEffect(() => {
+    localStorage.setItem("night-mode", JSON.stringify(theme));
+    if (theme) {
+      document.body.classList.add("night__mode");
+    } else {
+      document.body.classList.remove("night__mode");
+    }
+  }, [theme]);
   return (
     <div
-      onClick={() => setNightModeActive(!nightModeActive)}
+      onClick={() => {
+        setTheme(!theme);
+        document.body.style.position = "static";
+      }}
       className={`header__nav-icon night__mode-button ${
-        nightModeActive ? "night__mode-button-active" : ""
+        theme ? "night__mode-button-active" : ""
       }`}
     >
-      <i className={nightModeActive ? "ri-sun-line" : "ri-moon-line"}></i>
+      <i className={theme ? "ri-sun-line" : "ri-moon-line"}></i>
     </div>
   );
 }
